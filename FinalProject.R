751c90c1e3d7584da2e1db38e8993ed6ff0562c2
agbaca <adamgb.us@gmail.com>
2017-04-04 23:27:44 -0500

    xom

---
+++ FinalProject.R
@@ -0,0 +1,31 @@
+library(twitteR)
+library(RCurl)
+##Check other PC for extra packages###
+library("tseries")
+library(twitteR)
+setup_twitter_oauth("k2VRd8b9WOwP3bCPnzOJhY5BJ","zsfQh55pU4MwjDk9pcwuHu4dFrGpzWu9Yzr7nY3jvkkniIZsUN","793230364718215168-kYebJPAsXvcoW94PE8aS4rdj43ex5zJ","zYCgHZcuOYqmggErfm4Niy6fikCHzBwqkE78Wy5BSBfV3")
+library("tseries")
+snpxom<-get.hist.quote('XOM',quote="Close")
+length(snpxom)
+snprexom<-log(lag(snpxom))-log(snpxom)
+length(snprexom)
+snpvolxom<-sd(snprexom)*sqrt(250)*100
+snpvolxom
+volxom<-function(d,logrets)
+{
+  var=0
+  lam=0
+  varlist<-c()
+  for(r in logrets) {
+    lam=lam*(1-1/d)+1
+    var=(1-1/lam)*var+(1/lam)*r^2
+    varlist<-c(varlist,var)
+  }
+  sqrt(varlist)}
+volesxom<-volxom(10,snprexom)
+volesxom2<-volxom(30,snprexom)
+volesxom3<-volxom(100,snprexom)
+plot(volesxom,type="l")
+lines(volesxom2,type="l",col="red")
+lines(volesxom3,type="l",col="blue")
+searchTwitter("#makeamericagreatagain",n=100000000,since=2016-11-07,until=2016-11-08)
